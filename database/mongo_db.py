08f40510b33d8ae27cbf0874b2c8bda6047edcfd
Renzo Gabriel Fajardo <44675344+renzo611@users.noreply.github.com>
2025-04-03 12:36:41 -0300

    feat: add mongo db connection

---
+++ database/mongo_db.py
@@ -0,0 +1,20 @@
+import os
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MONGO_HOST = os.getenv("MONGO_HOST")
+MONGO_USER = os.getenv("MONGO_USER")
+MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
+MONGO_SCHEMA = os.getenv("MONGO_SCHEMA")
+
+def get_database():
+ 
+   CONNECTION_STRING = f"mongodb+srv://{MONGO_USER}:{MONGO_PASSWORD}@{MONGO_HOST}/{MONGO_SCHEMA}"
+ 
+   client = MongoClient(CONNECTION_STRING)
+ 
+   return client[MONGO_SCHEMA]
+  
+mongo_client = get_database()
